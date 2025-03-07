7350839fd5ab90486db8ebc4511c26067ccd4423
devamishaa <kumari.amisha@gmail.com>
2025-03-07 14:57:09 +0530

    fixed minor issues

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { watchEffect } from 'vue';
+import { defineProps, watchEffect } from 'vue';
 
 const props = defineProps(['post']);
 
@@ -9,12 +9,16 @@ watchEffect(() => {
 </script>
 
 <template>
-  <div v-if="post" class="flex flex-col justify-between w-full max-w-sm h-64 
+  <NuxtLink 
+    v-if="post"
+    :to="`/posts/${post.id}`"
+    class="flex flex-col justify-between w-full max-w-sm h-64 
            p-6 bg-gradient-to-br from-purple-500 via-indigo-500 to-blue-500 
-           text-white shadow-lg rounded-xl hover:shadow-2xl transition-transform 
-           transform hover:scale-105">
+           text-white shadow-lg rounded-xl hover:shadow-xl transition-transform 
+           transform hover:scale-105"
+  >
     <h2 class="text-2xl font-bold mb-3 line-clamp-2">Id: {{ post.id }}</h2>
     <h2 class="text-2xl font-bold mb-3 line-clamp-2">{{ post.title }}</h2>
     <p class="text-gray-200 text-sm line-clamp-3">{{ post.body }}</p>
-  </div>
+  </NuxtLink>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -17,18 +17,10 @@ const filteredPosts = computed(() => {
 <template>
   <div class="container mx-auto p-6">
     <h1 class="text-3xl font-bold mb-6">Posts</h1>
-    <input v-model="searchQuery" type="text" placeholder="Search posts..." 
-      class="mb-4 w-full p-3 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
-    />
-    <div class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-6 gap-2 lg:gap-1">
-      <NuxtLink 
-        v-for="post in filteredPosts"
-        :key="post.id"
-        :to="`/posts/${post.id}`"
-        class="block p-4"
-      >
-      <PostCard :post="post" />
-      </NuxtLink>
+    <input v-model="searchQuery" type="text" placeholder="Search posts..."
+      class="mb-4 w-full p-3 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400" />
+    <div class="grid grid-cols-1 mx-auto md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-6 gap-2 lg:gap-3">
+      <PostCard v-for="post in filteredPosts" :key="post.id" :post="post" />
     </div>
   </div>
 </template>
